ea2ea6e1d682579923abb8557c3543f8d565b6ca
Justin_Barfield <trumpet181006@gmail.com>
2024-03-22 16:14:14 -0500

    commit

--- MyPokeDexDB/PopulateDB.sql
+++ MyPokeDexDB/PopulateDB.sql
@@ -9,88 +9,15 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
-USE [MyPokedex]
+INSERT [dbo].[Role] ([RoleID], [RoleName]) VALUES (1, N'Justin')
 GO
-/****** Object:  Table [dbo].[Pokemon]    Script Date: 3/18/2024 1:17:04 PM ******/
-SET ANSI_NULLS ON
+INSERT [dbo].[Role] ([RoleID], [RoleName]) VALUES (2, N'Ethan')
 GO
-SET QUOTED_IDENTIFIER ON
+INSERT [dbo].[Role] ([RoleID], [RoleName]) VALUES (3, N'Luke')
 GO
-CREATE TABLE [dbo].[Pokemon](
-	[PokemonID] [int] NOT NULL,
-	[Dex Number] [int] NOT NULL,
-	[Name] [text] NOT NULL,
-	[Type] [text] NOT NULL,
-	[State Total] [int] NOT NULL,
-	[image URL] [varchar](50) NOT NULL,
-	[region] [text] NOT NULL,
- CONSTRAINT [PK_MyPokedex] PRIMARY KEY CLUSTERED 
-(
-	[PokemonID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+SET IDENTITY_INSERT [dbo].[User] ON 
 GO
-/****** Object:  Table [dbo].[Role]    Script Date: 3/18/2024 1:17:04 PM ******/
-SET ANSI_NULLS ON
+INSERT [dbo].[User] ([UserID], [First Name], [Last Name], [Email], [Password], [Last Login Time], [RoleID]) VALUES (4, N'Justin', N'Barfield', N'BarfieldJ@jacks.sfasu.edu', N'12345', NULL, 1)
 GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Role](
-	[RoleID] [int] NOT NULL,
-	[RoleName] [varchar](50) NULL,
- CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED 
-(
-	[RoleID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[User]    Script Date: 3/18/2024 1:17:04 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[User](
-	[UserID] [int] IDENTITY(1,1) NOT NULL,
-	[First Name] [varchar](50) NOT NULL,
-	[Last Name] [varchar](50) NOT NULL,
-	[Email] [varchar](50) NOT NULL,
-	[Password] [varchar](50) NOT NULL,
-	[Last Login Time] [datetime] NULL,
-	[RoleID] [int] NULL,
- CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
-(
-	[UserID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[UserPokemon]    Script Date: 3/18/2024 1:17:04 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[UserPokemon](
-	[UserPokemonID] [int] NOT NULL,
-	[UserID] [int] NOT NULL,
-	[PokemonID] [int] NOT NULL,
-	[RegisterDate] [varchar](50) NOT NULL,
- CONSTRAINT [PK_UserPokemon] PRIMARY KEY CLUSTERED 
-(
-	[UserPokemonID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[User]  WITH CHECK ADD  CONSTRAINT [FK_User_User] FOREIGN KEY([UserID])
-REFERENCES [dbo].[User] ([UserID])
-GO
-ALTER TABLE [dbo].[User] CHECK CONSTRAINT [FK_User_User]
-GO
-ALTER TABLE [dbo].[User]  WITH CHECK ADD  CONSTRAINT [FK_User_User1] FOREIGN KEY([RoleID])
-REFERENCES [dbo].[Role] ([RoleID])
-GO
-ALTER TABLE [dbo].[User] CHECK CONSTRAINT [FK_User_User1]
-GO
-ALTER TABLE [dbo].[UserPokemon]  WITH CHECK ADD  CONSTRAINT [FK_UserPokemon_UserPokemon] FOREIGN KEY([UserPokemonID])
-REFERENCES [dbo].[UserPokemon] ([UserPokemonID])
-GO
-ALTER TABLE [dbo].[UserPokemon] CHECK CONSTRAINT [FK_UserPokemon_UserPokemon]
+SET IDENTITY_INSERT [dbo].[User] OFF
 GO
---
+++ MyPokeDexWeb/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyPokeDexWeb.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+        
+        }
+    }
+}
