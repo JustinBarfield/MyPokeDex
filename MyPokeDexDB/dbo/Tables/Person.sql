80a64e90634923124a5f62f4f094e38bc8670b47
Justin_Barfield <trumpet181006@gmail.com>
2024-04-01 13:51:33 -0500

    schema update

---
+++ MyPokeDexDB/dbo/Tables/Person.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Person] (
+    [PersonID]  INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName] VARCHAR (50)  NOT NULL,
+    [LastName]  VARCHAR (50)  NOT NULL,
+    [Email]     VARCHAR (50)  NOT NULL,
+    [Password]  VARCHAR (MAX) NOT NULL,
+    [RoleID]    INT           NULL,
+    [Phone]     VARCHAR (50)  NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([PersonID] ASC),
+    CONSTRAINT [FK_User_User] FOREIGN KEY ([PersonID]) REFERENCES [dbo].[Person] ([PersonID]),
+    CONSTRAINT [FK_User_User1] FOREIGN KEY ([RoleID]) REFERENCES [dbo].[Role] ([RoleID])
+);
+
