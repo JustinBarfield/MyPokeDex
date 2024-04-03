a5413c7c5581dd54de411aeea6f1294748fdfcf9
Justin_Barfield <trumpet181006@gmail.com>
2024-04-03 13:26:58 -0500

    UpdatePersonLoginTime

--- MyPokeDexWeb/Pages/Account/Login.cshtml.cs
+++ MyPokeDexWeb/Pages/Account/Login.cshtml.cs
@@ -18,42 +18,82 @@ namespace MyPokeDexWeb.Pages
         {
             if (ModelState.IsValid)
             {
-				SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString());
-                string cmdText = "SELECT Password FROM  Person WHERE  Email=@email";
-                SqlCommand cmd = new SqlCommand(cmdText, conn);
-                conn.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
-                if(reader.HasRows)
-                {  
-                    reader.Read();
-                    if (!reader.IsDBNull(0))
-                    {
-                        string passwordHash = reader.GetString(0);
-                        if(SecurityHelper.VerifyPassword(LoginUser.Password, passwordHash)) {
-                            return RedirectToPage("Profile");
-                               
-                        }
-                        else
-                        {
-                            ModelState.AddModelError("Login Eror", "invalid credential, try again.");
-                            return Page();
-                        }
-                    }
-                    else
-                    {
-                        return Page();
-                    }
+                //check login credentials
+                if (ValidateCredentials())
+                {
+                    return RedirectToPage("Profile");
                 }
                 else
                 {
                     ModelState.AddModelError("Login Eror", "invalid credential, try again.");
                     return Page();
                 }
-                conn.Close();
+                //if credentials are valid
+
+
+
+                //redirect User to Profile
+
+                //otherwise, display error
+
             }
             else
             {
                 return Page();
+            }
+        }
+
+        private bool ValidateCredentials()
+        {
+            using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
+            {
+                string cmdText = "SELECT Password,PersonID FROM  Person WHERE  Email=@email";
+                SqlCommand cmd = new SqlCommand(cmdText, conn);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    if (!reader.IsDBNull(0))//the 0 is to selecct the correct index
+                    {
+                        string passwordHash = reader.GetString(0);
+                        if (SecurityHelper.VerifyPassword(LoginUser.Password, passwordHash))
+                        {
+                            //get the PersonID and use it to update the Person record
+                            int personID = reader.GetInt32(1);
+                            UpdatePersonLoginTime(personID);
+
+                            return true;
+
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+                conn.Close();
+            }
+        }
+
+        private void UpdatePersonLoginTime(int personID)
+        {
+            using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
+            {
+                string cmdText = "UPDATE Person SET LastLoginTime=@lastLoginTime Where PersonID=@personId";
+                SqlCommand cmd = new SqlCommand( cmdText, conn);
+                cmd.Parameters.AddWithValue("@lastLoginTime",DateTime.Now);
+                cmd.Parameters.AddWithValue("personId", personID);
+                conn.Open();
+                cmd.ExecuteNonQuery();
             }
         }
     }
