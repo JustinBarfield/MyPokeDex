07599811e14ce178447835fe5d1bf6ce33ae4db8
Justin_Barfield <trumpet181006@gmail.com>
2024-04-10 15:14:20 -0500

    added script statement

--- MyPokeDexDB/PopulateDB.sql
+++ MyPokeDexDB/PopulateDB.sql
@@ -9,17 +9,140 @@ Post-Deployment Script Template
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
-INSERT [dbo].[Role] ([RoleID], [RoleName]) VALUES (1, N'Justin')
+USE [MyPokedex]
 GO
-INSERT [dbo].[Role] ([RoleID], [RoleName]) VALUES (2, N'Ethan')
+/****** Object:  Table [dbo].[Person]    Script Date: 4/10/2024 3:13:41 PM ******/
+SET ANSI_NULLS ON
 GO
-INSERT [dbo].[Role] ([RoleID], [RoleName]) VALUES (3, N'Luke')
+SET QUOTED_IDENTIFIER ON
 GO
-SET IDENTITY_INSERT [dbo].[Person] ON 
+CREATE TABLE [dbo].[Person](
+	[PersonID] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [varchar](50) NOT NULL,
+	[LastName] [varchar](50) NOT NULL,
+	[Email] [varchar](50) NOT NULL,
+	[Password] [varchar](max) NOT NULL,
+	[RoleID] [int] NULL,
+	[Phone] [varchar](50) NULL,
+	[LastLoginTime] [date] NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[PersonID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-INSERT [dbo].[Person] ([PersonID], [FirstName], [LastName], [Email], [Password], [RoleID], [Phone], [LastLoginTime]) VALUES (1016, N'Justin', N'Barfield', N'justinbarfield02@gmail.com', N'$2a$13$mo6ORhK7tNT4EK/JsKdknePUD0yKJTOyD7nbqSDfZFI6LKpNFdHpe', 2, N'8178569902', CAST(N'2024-04-03' AS Date))
+/****** Object:  Table [dbo].[Pokemon]    Script Date: 4/10/2024 3:13:41 PM ******/
+SET ANSI_NULLS ON
 GO
-INSERT [dbo].[Person] ([PersonID], [FirstName], [LastName], [Email], [Password], [RoleID], [Phone], [LastLoginTime]) VALUES (1017, N'Tracey', N'Barfield', N'tracey@gmail.com', N'$2a$13$1VactTR55Uti1h3c7LL9jOvLz21pxFh8x/kwB297kqBp19gQJF0Ue', 2, N'8178569902', NULL)
+SET QUOTED_IDENTIFIER ON
 GO
-SET IDENTITY_INSERT [dbo].[Person] OFF
+CREATE TABLE [dbo].[Pokemon](
+	[PokemonID] [int] NOT NULL,
+	[Dex Number] [int] NOT NULL,
+	[Name] [text] NOT NULL,
+	[TypeID] [int] NULL,
+	[State Total] [int] NOT NULL,
+	[image URL] [varchar](50) NULL,
+	[RegionID] [int] NULL,
+	[Height] [text] NULL,
+	[Weight] [text] NULL,
+	[Audio] [varchar](50) NULL,
+ CONSTRAINT [PK_MyPokedex] PRIMARY KEY CLUSTERED 
+(
+	[PokemonID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Region]    Script Date: 4/10/2024 3:13:41 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Region](
+	[RegionID] [int] NOT NULL,
+	[RegionName] [varchar](50) NULL,
+ CONSTRAINT [PK_Region] PRIMARY KEY CLUSTERED 
+(
+	[RegionID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Role]    Script Date: 4/10/2024 3:13:41 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Role](
+	[RoleID] [int] NOT NULL,
+	[RoleName] [varchar](50) NULL,
+ CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED 
+(
+	[RoleID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Type]    Script Date: 4/10/2024 3:13:41 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Type](
+	[TypeID] [int] NOT NULL,
+	[TypeName] [varchar](50) NULL,
+ CONSTRAINT [PK_Type] PRIMARY KEY CLUSTERED 
+(
+	[TypeID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UserPokemon]    Script Date: 4/10/2024 3:13:41 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserPokemon](
+	[UserPokemonID] [int] NOT NULL,
+	[UserID] [int] NOT NULL,
+	[PokemonID] [int] NOT NULL,
+	[RegisterDate] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_UserPokemon] PRIMARY KEY CLUSTERED 
+(
+	[UserPokemonID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Person]  WITH CHECK ADD  CONSTRAINT [FK_User_User] FOREIGN KEY([PersonID])
+REFERENCES [dbo].[Person] ([PersonID])
+GO
+ALTER TABLE [dbo].[Person] CHECK CONSTRAINT [FK_User_User]
+GO
+ALTER TABLE [dbo].[Person]  WITH CHECK ADD  CONSTRAINT [FK_User_User1] FOREIGN KEY([RoleID])
+REFERENCES [dbo].[Role] ([RoleID])
+GO
+ALTER TABLE [dbo].[Person] CHECK CONSTRAINT [FK_User_User1]
+GO
+ALTER TABLE [dbo].[Pokemon]  WITH CHECK ADD  CONSTRAINT [FK_Pokemon_Pokemon] FOREIGN KEY([PokemonID])
+REFERENCES [dbo].[Pokemon] ([PokemonID])
+GO
+ALTER TABLE [dbo].[Pokemon] CHECK CONSTRAINT [FK_Pokemon_Pokemon]
+GO
+ALTER TABLE [dbo].[Pokemon]  WITH CHECK ADD  CONSTRAINT [FK_Pokemon_Pokemon1] FOREIGN KEY([PokemonID])
+REFERENCES [dbo].[Pokemon] ([PokemonID])
+GO
+ALTER TABLE [dbo].[Pokemon] CHECK CONSTRAINT [FK_Pokemon_Pokemon1]
+GO
+ALTER TABLE [dbo].[Pokemon]  WITH CHECK ADD  CONSTRAINT [FK_Pokemon_Region] FOREIGN KEY([RegionID])
+REFERENCES [dbo].[Region] ([RegionID])
+GO
+ALTER TABLE [dbo].[Pokemon] CHECK CONSTRAINT [FK_Pokemon_Region]
+GO
+ALTER TABLE [dbo].[Pokemon]  WITH CHECK ADD  CONSTRAINT [FK_Pokemon_Type] FOREIGN KEY([TypeID])
+REFERENCES [dbo].[Type] ([TypeID])
+GO
+ALTER TABLE [dbo].[Pokemon] CHECK CONSTRAINT [FK_Pokemon_Type]
+GO
+ALTER TABLE [dbo].[UserPokemon]  WITH CHECK ADD  CONSTRAINT [FK_UserPokemon_UserPokemon] FOREIGN KEY([UserPokemonID])
+REFERENCES [dbo].[UserPokemon] ([UserPokemonID])
+GO
+ALTER TABLE [dbo].[UserPokemon] CHECK CONSTRAINT [FK_UserPokemon_UserPokemon]
 GO
